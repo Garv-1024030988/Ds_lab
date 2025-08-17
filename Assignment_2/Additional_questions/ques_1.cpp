fe7c84bc1a28ddfd83fea09570999d8e8d5ef1b8
Garv-1024030988 <gbansal_be24@thapar.edu>
2025-08-17 22:56:03 +0530

    Create ques_1.cpp

---
+++ Assignment_2/Additional_questions/ques_1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[100];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int k;
+    cin >> k;
+
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (abs(arr[i] - arr[j]) == k) {
+                count++;
+            }
+        }
+    }
+
+    cout << count;
+    return 0;
+}
