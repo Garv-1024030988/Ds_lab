cdfeb2584f78917919c59e5c1d7dd8bf00133923
Garv-1024030988 <gbansal_be24@thapar.edu>
2025-08-18 15:22:11 +0530

    Create ques_5(a).cpp

---
+++ Assignment_2/ques_5(a).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int n;
+    cout<<"enter number of elements in matrix :";
+    cin>>n;
+
+    int arr[n];
+    cout<<"enter numbers :";
+    for(int i = 0 ; i < n ;i++){
+        cin>>arr[i];
+    }
+
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < n ; j++){
+            if( i == j){
+                cout<<arr[i]<<" ";
+            }else{
+                cout<<"0 ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
