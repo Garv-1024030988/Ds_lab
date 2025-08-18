1419430e9e81dc232fb0b6ebd66dcdde1624f8a9
Garv-1024030988 <gbansal_be24@thapar.edu>
2025-08-18 19:21:48 +0530

    Create ques_5(d).cpp

---
+++ Assignment_2/ques_5(d).cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter the size of matrix :";
+    cin >> n;
+
+    int k = (n * (n + 1)) / 2;
+    int arr[k];
+
+    cout << "enter the elements in row major order :";
+    for (int i = 0; i < k; i++)
+    {
+        cin >> arr[i];
+    }
+
+    k = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (j >= i)
+            {
+                cout << arr[k] << " ";
+                k++;
+            }
+            else
+            {
+                cout << "0 ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
