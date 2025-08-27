bb82ed4f68b900679a4b107772b2b0451b9f4a96
Garv-1024030988 <gbansal_be24@thapar.edu>
2025-08-27 21:54:58 +0530

    Create ques_4(a).cpp

---
+++ Assignment_2/ques_4(a).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s1, s2;
+    cout << "Enter first string: ";
+    cin >> s1;
+    cout << "Enter second string: ";
+    cin >> s2;
+
+    string result = s1 + s2;   // Concatenation
+    cout << "Concatenated string: " << result << endl;
+
+    return 0;
+}
