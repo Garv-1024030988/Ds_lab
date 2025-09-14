8f8e3ee305153427a2ea080b47067f3b4d1885ee
Garv-1024030988 <gbansal_be24@thapar.edu>
2025-09-14 15:17:34 +0530

    Create ques_2.cpp

---
+++ Assignment_2/Additional_questions/ques_2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+bool canSplit(string &st){
+    int n=st.length();
+
+    if(n<3) return false;
+
+    vector<int>freq(26,0);
+    for(char c:st) freq[c-'a']++;
+    for(int f : freq){
+        if(f>=3) return true;
+    }
+
+
+    return false;
+}
+
+
+int main(){
+    
+    string s="ababa";
+    cout<<(canSplit(s) ? "Yes" : "No");
+    
+    return 0;
+}
