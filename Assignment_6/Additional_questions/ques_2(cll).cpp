b994e65dd53feaaf26d4f24ad35c7f12dd4f2d20
Garv-1024030988 <gbansal_be24@thapar.edu>
2025-10-26 23:34:05 +0530

    ques_2(cll).cpp

--- Assignment_6/Additional_questions/ques_2(cll).cpp
+++ Assignment_6/Additional_questions/ques_2(cll).cpp
@@ -1,96 +1,121 @@
 #include <iostream>
 using namespace std;
 
-struct CNode {
+class Node {
+public:
     int data;
-    CNode* next;
-    CNode(int val) : data(val), next(nullptr) {}
+    Node* next;
+    Node* prev;
+    
+    Node(int value) {
+        data = value;
+        next = nullptr;
+        prev = nullptr;
+    }
 };
 
-void insertCircular(CNode*& head, int val) {
-    CNode* newNode = new CNode(val);
-    if (!head) {
-        head = newNode;
-        head->next = head;
-        return;
+bool hasEvenParity(int num) {
+    int count = 0;
+    while (num > 0) {
+        count += num & 1;
+        num >>= 1;
     }
-    CNode* temp = head;
-    while (temp->next != head) temp = temp->next;
-    temp->next = newNode;
-    newNode->next = head;
+    return count % 2 == 0;
 }
 
-void removeEvenCircular(CNode*& head) {
-    if (!head) return;
+class CircularLinkedList {
+private:
+    Node* head;
 
-    CNode* curr = head;
-    CNode* prev = nullptr;
-    int countChecked = 0, total = 0;
-    CNode* t = head;
+public:
+    CircularLinkedList() {
+        head = nullptr;
+    }
 
-    do {
-        total++;
-        t = t->next;
-    } while (t != head);
-
-    while (countChecked < total && head) {
-        countChecked++;
-        if (curr->data % 2 == 0) {
-            if (curr->next == curr) {
-                delete curr;
-                head = nullptr;
-                return;
-            }
-            if (curr == head) {
-                CNode* tail = head;
-                while (tail->next != head) tail = tail->next;
-                head = head->next;
-                tail->next = head;
-                CNode* toDelete = curr;
-                curr = head;
-                delete toDelete;
-                prev = tail;
-            } else {
-                prev->next = curr->next;
-                CNode* toDelete = curr;
-                curr = curr->next;
-                delete toDelete;
-            }
-        } else {
-            prev = curr;
-            curr = curr->next;
+    void insertAtEnd(int value) {
+        Node* newNode = new Node(value);
+        
+        if (head == nullptr) {
+            head = newNode;
+            head->next = head;
+            return;
         }
+        
+        Node* last = head;
+        while (last->next != head) {
+            last = last->next;
+        }
+        
+        last->next = newNode;
+        newNode->next = head;
     }
-}
 
-void displayCircular(CNode* head) {
-    if (!head) {
-        cout << "List empty" << endl;
-        return;
+    void removeEvenParityNodes() {
+        if (head == nullptr) return;
+        
+        Node* current = head;
+        Node* previous = nullptr;
+        
+        do {
+            Node* nextNode = current->next;
+            
+            if (hasEvenParity(current->data)) {
+                if (previous == nullptr) {
+                    if (current->next == head) {
+                        head = nullptr;
+                    } else {
+                        Node* last = head;
+                        while (last->next != head) {
+                            last = last->next;
+                        }
+                        last->next = head->next;
+                        head = head->next;
+                    }
+                } else {
+                    previous->next = current->next;
+                }
+                
+                delete current;
+                current = nextNode;
+            } else {
+                previous = current;
+                current = nextNode;
+            }
+            
+        } while (current != head && head != nullptr);
     }
-    CNode* temp = head;
-    do {
-        cout << temp->data << " ";
-        temp = temp->next;
-    } while (temp != head);
-    cout << endl;
-}
+
+    void display() {
+        if (head == nullptr) {
+            cout << "Empty list\n";
+            return;
+        }
+        
+        Node* current = head;
+        do {
+            cout << current->data << " ";
+            current = current->next;
+        } while (current != head);
+        cout << "\n";
+    }
+};
 
 int main() {
-    CNode* cHead = nullptr;
-    insertCircular(cHead, 12);
-    insertCircular(cHead, 7);
-    insertCircular(cHead, 18);
-    insertCircular(cHead, 5);
-    insertCircular(cHead, 20);
-
-    cout << "Original Circular Linked List: ";
-    displayCircular(cHead);
-
-    removeEvenCircular(cHead);
-
-    cout << "After removing even nodes (CLL): ";
-    displayCircular(cHead);
-
+    CircularLinkedList cll;
+    
+    cout << "Circular Linked List:\n";
+    cll.insertAtEnd(1);
+    cll.insertAtEnd(2);
+    cll.insertAtEnd(3);
+    cll.insertAtEnd(4);
+    cll.insertAtEnd(5);
+    
+    cout << "Original list: ";
+    cll.display();
+    
+    cll.removeEvenParityNodes();
+    cout << "After removing even parity nodes: ";
+    cll.display();
+    
     return 0;
 }
